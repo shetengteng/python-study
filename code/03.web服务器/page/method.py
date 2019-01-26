b39d683c019a712d9d4a2961908d567898c51eb6
shetengteng <work_stt@163.com>
2019-01-26 15:11:05 +0800

    正则表达式学习

--- code/03.web服务器/page/method.py
+++ code/03.web服务器/page/method.py
@@ -1,15 +1,15 @@
 # coding:utf-8
 import time
-from MyWebDecorator import web_access
+from MyWebDecorator import web_access,ret_json
 
 # 时间运行函数，返回时间
 @web_access
 def show_time(param):
-
     print(param)
     # 返回response body数据
     return time.ctime()
 
-@web_access(ret_type="json")
+@web_access
+@ret_json
 def show_param(param):
     return param
